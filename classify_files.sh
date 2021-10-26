af06577be1887afb217b690134e27a81c51df1f1
karimbakhshamiry <45913229+karimbakhshamiry@users.noreply.github.com>
2021-10-26 14:32:48 +0430

    Add files via upload
    creation of the main script which is used to categorize files

---
+++ classify_files.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+mkdir Documents Videos Pictures Music
+
+#move all images to Pictures directory
+mv *jpeg Pictures
+mv *gif Pictures
+mv *jpg Pictures
+mv *webp Pictures
+
+#move all documents to Documents folder
+mv *doc Documents
+mv *docx Documents
+mv *odt Documents
+mv *pdf Documents
+mv *rtf Documents
+mv *txt Documents
+
+#move all audio files to Music folders
+mv *mp3 Music
+mv *aiff Music
+mv *aac Music
+mv *wav Music
+
+#move all video files to Videos folder
+mv *mp4 Videos
+mv *avi Videos
+mv *mpeg Videos
+mv *wmv Videos
+
+
+echo "All files moved to their respective directories"
+mkdir Scripts
+mv *sh Scripts
