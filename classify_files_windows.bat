b17c27876af30d6fca0a99709815a7295f7f1157
karimbakhshamiry <45913229+karimbakhshamiry@users.noreply.github.com>
2021-10-26 14:53:40 +0430

    create classify_files_windows.bat
    created the script for windows user

---
+++ classify_files_windows.bat
@@ -0,0 +1,27 @@
+mkdir Documents Videos Pictures Music
+
+#move all images to Pictures directory
+move *jpeg Pictures
+move *gif Pictures
+move *jpg Pictures
+move *webp Pictures
+
+#move all documents to Documents folder
+move *doc Documents
+move *docx Documents
+move *odt Documents
+move *pdf Documents
+move *rtf Documents
+move *txt Documents
+
+#move all audio files to Music folders
+move *mp3 Music
+move *aiff Music
+move *aac Music
+move *wav Music
+
+#move all video files to Videos folder
+move *mp4 Videos
+move *avi Videos
+move *mpeg Videos
+move *wmv Videos
